60efa24c47fa90e4d20c70f2e92f72920174761d
Jotas <jproodian721@gmail.com>
2022-03-22 12:36:00 -0500

    created indexeddb connection

---
+++ public/assets/js/idb.js
@@ -0,0 +1,38 @@
+// create variable to hold db connection
+let db;
+
+// establish a connection to IndexedDB db called 'pizza_hunt' and set it to version 1
+const request = indexedDB.open('pizza_hunt', 1);
+
+// this event will emit if the database version changes
+request.onupgradeneeded = function(event) {
+    // save a reference to the db
+    const db = event.target.result;
+    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_pizza', { autoIncrement: true });
+}
+
+// upon a successful
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradeneeded event above) or simply established a connection, save reference to db in global variable
+    if (navigator.onLine) {
+        // uploadPizza();
+    }
+};
+
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access the object store for `new_pizza`
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    // add record to your store with add method
+    pizzaObjectStore.add(record);
+}
